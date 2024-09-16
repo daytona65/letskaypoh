789a38298f778615b25fb11dc4f5c7efc4dd456f
daytona65 <niclim65@gmail.com>
2024-09-16 23:03:47 +0800

    move implementation to api

--- server/server.py
+++ server/server.py
@@ -5,6 +5,7 @@ from dotenv import dotenv_values
 from bson import json_util, ObjectId
 from config import app, db
 from models import User, Senior, Visit
+from api.user_service import *
 import json
 
 config = dotenv_values(".env")
@@ -16,64 +17,19 @@ counter_collection = db['counters']
 
 # 1
 @app.route("/users", methods=["GET"])
-def get_all_users():
-    users = list(user_collection.find())
-    return Response(json.dumps(users, default=str), mimetype="application/json")
+def users():
+    return get_all_users()
 
 # 2
 @app.route("/user", methods=["GET"])
-def get_user():
+def user():
     user_id = int(request.args.get('id'))
-    user = list(user_collection.find({"user_id": user_id}))
-    if user is None:
-        print("User not found!")
-        return jsonify({"error": "User not found"}), 404
-    return json.dumps(user[0], default=json_util.default)
+    return get_user(user_id)
 
 # 3
 @app.route("/create_user", methods=["POST"])
 def create_user():
-    user_id = counter_collection.find_one_and_update(
-        {"id": "user_count"},
-        {"$inc": {"count": 1}},
-        return_document=True,
-        upsert=True
-    )["count"]
-    data = request.json
-    nric = data.get("nric")
-    name = data.get("name")
-    gender = data.get("gender")
-    age = data.get("age")
-    postal_code = data.get("postal_code")
-    address = data.get("address")
-    languages = data.get("languages")
-    profile_img = data.get("profile_img")
-    email = data.get("email")
-    password = data.get("password")
-    
-    
-    if not data or not nric or not name or not gender or not age or not postal_code or not address or not languages or not profile_img or not email or not password or not isinstance(languages, list):
-        return jsonify({"error": "Request body error. All fields are required. Languages is a list."}), 400
-    
-    try:
-        new_user = {
-                "user_id": user_id,
-                "nric": nric,
-                "name": name,
-                "gender": gender,
-                "age": age,
-                "postal_code": postal_code,
-                "address": address,
-                "languages": languages,
-                "profile_img": profile_img,
-                "email": email,
-                "password": password
-        }
-        user_collection.insert_one(new_user)
-    except Exception as e:
-        return jsonify({"message": str(e)}), 400
-
-    return jsonify({"message": "User created!", "new_user": str(new_user)}), 201
+    return create_new_user()
 
 # 4
 @app.route("/seniors", methods=["GET"])
