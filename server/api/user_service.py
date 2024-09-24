de68abe581edb686ab366b54f0f7907b2421f518
daytona65 <niclim65@gmail.com>
2024-09-24 15:17:15 +0800

    User auth

--- server/api/user_service.py
+++ server/api/user_service.py
@@ -32,8 +32,12 @@ def register_user():
     except Exception as e:
         return Response(json.dumps({"message": str(e)}), mimetype="application/json", status=500)
 
-    access_token = create_access_token(identity={"user_id": user_id})
-    return jsonify({"message": "User registered successfully!", "access_token": access_token}), 201
+    try:
+        # Create access token with user_id
+        access_token = create_access_token(identity={"user_id": user_id})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    return jsonify({"message": "User registered successfully!"}), 201
 
 def login_user():
     data = request.json
@@ -52,7 +56,7 @@ def login_user():
     if not user or not bcrypt.check_password_hash(user['password'], password):
         return Response(json.dumps({"error": "Invalid credentials"}), mimetype='application/json', status=401)
     
-    access_token = create_access_token(identity=user['user_id'])
+    access_token = create_access_token(identity={"user_id": user_id})
     return jsonify({"message": "User login successfully!"}), 201
 
 def get_all_users():
